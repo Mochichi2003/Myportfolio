7d2cd6ecefee65abba146f2fd6777e5d810f8d5d
Mochichi <moyurusuto@gmail.com>
2019-12-31 22:33:45 +0900

    2019/12/31 22:33

--- src/scr.js
+++ src/scr.js
@@ -95,3 +95,28 @@ function headerbar(params) {
     $("header").css("top", "0px");
   }
 }
+
+
+// メールを暗号化
+
+//   function converter(M) {
+//     var str = "", str_as = "";
+//     for (var i = 0; i < M.length; i++) {
+//       str_as = M.charCodeAt(i);
+//       str += String.fromCharCode(str_as + 1);
+//     }
+//     return str;
+//   }
+// function mail_to(k_1, k_2) {
+//   eval(String.fromCharCode(108, 111, 99, 97, 116, 105, 111, 110, 46, 104, 114, 101, 102, 32,
+//     61, 32, 39, 109, 97, 105, 108, 116, 111, 58)
+//     + escape(k_1) +
+//     converter(String.fromCharCode(108, 110, 120, 116, 113, 116, 114, 116, 115, 110, 63, 102, 108, 96, 104, 107, 45, 98, 110, 108,
+//       62, 114, 116, 97, 105, 100, 98, 115, 60))
+//     + escape(k_2) + "'");
+// }
+// document.write('<a href=JavaScript:mail_to("","")><img src="svg/メールの無料アイコンその8.svg" alt="メール"><\/a>');
+// $(function () {
+//   $(".sns-icon").append('<a href=JavaScript:mail_to("","")><img src="svg/メールの無料アイコンその8.svg" alt="メール"><\/a>');
+// });
+// //-->
